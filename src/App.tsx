94edfa153e3e7cbe14e7842b87796bf060c54604
mikewegele <mike.wegele@iav.de>
2025-03-18 15:22:20 +0100

    test new workflow

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsedrgergex</code> and save to test HMR
+          Edit <code>src/App.tsedrgergedfgdrgerdx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
